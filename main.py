3323133f265adf4783f6ae58111c55736cfb18c7
温清梦 <flidwala@gmail.com>
2024-11-23 17:38:37 +0800

    fix bug

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ Datasets, Dataset_ids = split_dataset(P.Annotation_PATH, P.Set_names, P.Set_rati
 for dataset, dataset_id, set_name in zip(Datasets, Dataset_ids, P.Set_names):
     print(f"create {set_name} dataset...")
 
-    with open(os.path.join(P.Output_PATH, f"{set_name}.txt"), "w", encoding="utf-8") as f:
-        f.writelines([os.path.join(P.Output_image_path, set_name, f"{d}.jpg")+"\n" for d in dataset_id])
+    used_data = []
 
     for file, file_id in tqdm(zip(dataset, dataset_id), total=len(dataset)):
         # 根据xml文件编写yolo格式的标签
@@ -21,6 +20,7 @@ for dataset, dataset_id, set_name in zip(Datasets, Dataset_ids, P.Set_names):
         # 如果size中的宽或高标注为0, 则无法计算label，跳过该数据
         if not labels:
             continue
+        used_data.append(file_id)
 
         # 保存标签到指定路径
         with open(os.path.join(P.Output_label_path, set_name, f"{file_id}.txt"), 'w', encoding='utf-8') as label_file:
@@ -28,3 +28,6 @@ for dataset, dataset_id, set_name in zip(Datasets, Dataset_ids, P.Set_names):
 
         # 复制/剪切图片到指定路径
         create_image(os.path.join(P.Image_PATH, f"{file}.jpg"), os.path.join(P.Output_image_path, set_name, f"{file_id}.jpg"), P.MODE)
+
+    with open(os.path.join(P.Output_PATH, f"{set_name}.txt"), "w", encoding="utf-8") as f:
+        f.writelines([os.path.join("./images", set_name, f"{d}.jpg")+"\n" for d in used_data])
